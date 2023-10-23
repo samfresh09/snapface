63c480295f3cb404cc4d7b89ff9b41365172f132
samfresh09 <samtou.gnazouyoufei@ipnetinstitute.com>
2023-10-23 14:34:55 +0000

    creation du model faceSnap

---
+++ src/app/models/faceSnap.model.ts
@@ -0,0 +1,12 @@
+export class FaceSnap {
+   
+    constructor(
+        public titre:string,
+        public description:string,
+        public like:number,
+        public image:string,
+        public date: Date,
+        ) {
+    
+}
+}
